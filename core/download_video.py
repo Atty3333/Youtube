d0f2c85e6b7cf901ffe79b9609de0da0b970c336
Atty3333 <157931044+Atty3333@users.noreply.github.com>
2025-08-01 02:30:02 -0700

    Update download_video.py

--- core/download_video.py
+++ core/download_video.py
@@ -44,6 +44,8 @@ def search_and_download(keyword, output_path=os.path.normpath('clips/source_vide
 
 def download_video(url, output_path,PROJECT_ROOT):
     try:
+            cookie=os.path.join(PROJECT_ROOT, 'youtube.com_cookies.txt')
+            print(cookie)
             ydl_opts = {
                 'format': 'bestvideo[height=360][ext=mp4]+bestaudio[ext=m4a]/mp4',  # 360p video with audio, fallback to mp4
                 'outtmpl': output_path,
@@ -54,7 +56,7 @@ def download_video(url, output_path,PROJECT_ROOT):
                 "fragment_retries": 10,
                 "socket_timeout": 30,
                 "noplaylist": True,
-                'cookies': os.path.join(PROJECT_ROOT, 'youtube.com_cookies.txt'),
+                'cookies':cookie,
 
         
                 'postprocessors': [
